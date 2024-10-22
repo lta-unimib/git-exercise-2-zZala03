99ccef5832c1f3c7166f10b00b23ff5109eb0187
Andrea Silvano Lazzarin <a.lazzarin10@campus.unimib.it>
2024-10-22 15:12:48 +0200

    Sono il primo developer e ho modificato una riga di codice

--- src/gitExercise2/main/HelloGit.java
+++ src/gitExercise2/main/HelloGit.java
@@ -3,6 +3,6 @@ package gitExercise2.main;
 public class HelloGit {
 	public static void main (String [] args) {
 		System.out.println("Hello git!!!");
-		System.out.println("Questa è una stampa!")
+		System.out.println("Questa è una nuovissima stampa, che serve per simulare la modifica simultanea di una riga di codice!");
 	}
 }
